54f3c4b581acbbb79648f21f6a8c8f3e319cb448
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-03-14 11:47:29 -0300

    numbers

--- AtividadeArray.js
+++ AtividadeArray.js
@@ -2,8 +2,8 @@
 let a = [1, 2, 3,];
 let b = [4, 5, 6,];
 
-let numbers = a.concat(b);
-console.log(numbers);
+let sequencia = a.concat(b);
+console.log(sequencia);
 
 // .Slice retorna uma copia de array sem algumas index (sempre tira index em Array sobre porções).
 
@@ -11,3 +11,10 @@ let frutas = ["Banana", "Uva", "Morango", "Laranja", "jambo"];
 
 let melhoresFrutas = frutas.slice(1, 3);
 console.log(melhoresFrutas);
+
+// .Sort exibe números de Array em ordem crescente ou decrescente
+
+let numbers = [5, 4, 3, 2, 1];
+numbers.sort()
+
+console.log(numbers);

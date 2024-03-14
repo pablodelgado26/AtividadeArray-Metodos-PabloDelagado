f9c77bd17b3a0018bab6b56a6492366f39219775
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-03-14 11:28:45 -0300

    Slice

--- AtividadeArray.js
+++ AtividadeArray.js
@@ -1,6 +1,13 @@
-// Contact unir valores ou Arrays e retorna uma nova Array com todos valores com a ordem passada.
+// .Contact unir valores ou Arrays e retorna uma nova Array com todos valores com a ordem passada.
 let a = [1, 2, 3,];
 let b = [4, 5, 6,];
 
 let numbers = a.concat(b);
 console.log(numbers);
+
+// .Slice retorna uma copia de array sem algumas index (sempre tira index em Array sobre porções).
+
+let frutas = ["Banana", "Uva", "Morango", "Laranja", "jambo"];
+
+let melhoresFrutas = frutas.slice(1, 3);
+console.log(melhoresFrutas);
